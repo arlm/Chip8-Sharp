b35db9ba216f3c5218c7a310c6e667ffd4087c18
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@keyidentity.com>
2018-12-12 16:43:52 +0100

    Initial SDL2 support

--- Chip8.Sharp/Chip8/Program.cs
+++ Chip8.Sharp/Chip8/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using Chip8.Core;
+using SDL2;
 
 namespace Chip8
 {
     class Program
     {
         // In this example we assume you will create a separate class to handle the opcodes.
-        static CPU myChip8;
+        private static CPU myChip8;
+
+        private const int WIDTH = 1024;
+        private const int HEIGHT = 768;
+        private static Random r = new Random();
+
+        private static IntPtr rendererPtr;
+        private static IntPtr windowPtr;
 
         static void Main(string[] args)
         {
+
+            var result = SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
+
             // Setup the graphics (window size, display mode, etc) 
             SetupGraphics();
 
@@ -20,7 +31,7 @@ namespace Chip8
             myChip8 = new CPU();
 
             // Load (copy) the game into the memory
-            myChip8.LoadGame("pong");
+            myChip8.LoadGame("pong2.c8");
 
             // Emulation loop
             for (; ; )
@@ -42,22 +53,115 @@ namespace Chip8
                 // Store key press state (Press and Release)
                 // If we press or release a key, we should store this state in the part that emulates the keypad
                 myChip8.SetKeys();
-            }
-        }
 
-        private static void DrawGraphics()
-        {
-            throw new NotImplementedException();
+                // 60 Hz = 1000 ms /60 = 16.666 ms  => approx. 17 ms (17 ms * 60 = 1020 ms)
+                SDL.SDL_Delay(17);
+            }
+
+            Quit();
         }
 
         private static void SetupGraphics()
         {
-            throw new NotImplementedException();
+            windowPtr = SDL.SDL_CreateWindow("CHIP-8", 0, 0, WIDTH, HEIGHT, SDL.SDL_WindowFlags.SDL_WINDOW_ALLOW_HIGHDPI);
+            SDL.SDL_ShowWindow(windowPtr);
+
+            rendererPtr = SDL.SDL_GetRenderer(windowPtr);
+
+            SDL.SDL_SetRenderDrawColor(rendererPtr, 0, 0, 0, 255);
+            SDL.SDL_RenderClear(rendererPtr);
+
+            SDL.SDL_RenderPresent(rendererPtr);
         }
 
         private static void SetupInput()
         {
-            throw new NotImplementedException();
+            while (SDL.SDL_PollEvent(out var evt) > 0)
+            {
+                switch (evt.type)
+                {
+                    case SDL.SDL_EventType.SDL_QUIT:
+                        Quit();
+                        break;
+
+                    case SDL.SDL_EventType.SDL_SENSORUPDATE:
+                        break;
+
+                    case SDL.SDL_EventType.SDL_KEYDOWN:
+                        break;
+                    case SDL.SDL_EventType.SDL_KEYUP:
+                        break;
+
+                    case SDL.SDL_EventType.SDL_MOUSEMOTION:
+                        break;
+                    case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
+                        break;
+                    case SDL.SDL_EventType.SDL_MOUSEBUTTONUP:
+                        break;
+                    case SDL.SDL_EventType.SDL_MOUSEWHEEL:
+                        break;
+
+                    case SDL.SDL_EventType.SDL_FINGERMOTION:
+                        break;
+                    case SDL.SDL_EventType.SDL_FINGERDOWN:
+                        break;
+                    case SDL.SDL_EventType.SDL_FINGERUP:
+                        break;
+                    case SDL.SDL_EventType.SDL_MULTIGESTURE:
+                        break;
+
+                    case SDL.SDL_EventType.SDL_JOYDEVICEADDED:
+                        break;
+                    case SDL.SDL_EventType.SDL_JOYDEVICEREMOVED:
+                        break;
+                    case SDL.SDL_EventType.SDL_JOYBUTTONDOWN:
+                        break;
+                    case SDL.SDL_EventType.SDL_JOYBUTTONUP:
+                        break;
+                    case SDL.SDL_EventType.SDL_JOYHATMOTION:
+                        break;
+                    case SDL.SDL_EventType.SDL_JOYAXISMOTION:
+                        break;
+                    case SDL.SDL_EventType.SDL_JOYBALLMOTION:
+                        break;
+
+                    case SDL.SDL_EventType.SDL_CONTROLLERDEVICEADDED:
+                        break;
+                    case SDL.SDL_EventType.SDL_CONTROLLERDEVICEREMOVED:
+                        break;
+                    case SDL.SDL_EventType.SDL_CONTROLLERDEVICEREMAPPED:
+                        break;
+                    case SDL.SDL_EventType.SDL_CONTROLLERBUTTONDOWN:
+                        break;
+                    case SDL.SDL_EventType.SDL_CONTROLLERBUTTONUP:
+                        break;
+                    case SDL.SDL_EventType.SDL_CONTROLLERAXISMOTION:
+                        break;
+                }
+            }
+        }
+
+        private static void Quit()
+        {
+            SDL.SDL_DestroyRenderer(rendererPtr);
+            SDL.SDL_DestroyWindow(windowPtr);
+            SDL.SDL_Quit();
+        }
+
+        private static void DrawGraphics()
+        {
+            SDL.SDL_SetRenderDrawColor(rendererPtr, 255, 255, 255, 255);
+
+            var rect = new SDL.SDL_Rect
+            {
+                x = 10,
+                y = 10,
+                w = 10,
+                h = 10
+            };
+
+            SDL.SDL_RenderDrawRect(rendererPtr, ref rect);
+            SDL.SDL_RenderPresent(rendererPtr);
         }
     }
 }
